65a3136a96c4a1dce6bccc262369643ed7f3bc6f
Max Yakushev <m.yakushev23@gmail.com>
2022-11-05 21:01:47 +0200

    tetris: (feat) right-left player moving

--- src/components/game/tetris.tsx
+++ src/components/game/tetris.tsx
@@ -1,10 +1,27 @@
 import { Board } from '@/components';
-import { buildBoard } from '@/utils';
+import { useBoard, usePlayer } from '@/hooks';
 
 export const Tetris = () => {
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [board, setBoard] = useBoard(player);
+
+  const movePlayer = (dir: number) => {
+    updatePlayerPos({ x: dir, y: 0, collided: false });
+  };
+
+  const move = ({ keyCode }: { keyCode: number }): void => {
+    if (keyCode === 37) {
+      movePlayer(-1);
+    } else if (keyCode === 39) {
+      movePlayer(1);
+    }
+  };
+
+  console.log(setBoard, resetPlayer);
+
   return (
-    <div className="Tetris">
-      <Board board={buildBoard()} />
+    <div className="Tetris" role="button" tabIndex={0} onKeyDown={move}>
+      <Board board={board} />
     </div>
   );
 };
